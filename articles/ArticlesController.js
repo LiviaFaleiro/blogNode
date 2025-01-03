ea5bbedd791bcb363d1f81fdcc6c5a4ca13b25bc
Lívia <116227611+lihbolivia@users.noreply.github.com>
2025-01-03 01:35:36 -0300

    `Added Category model import and updated admin articles new route to render categories`

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../categories/category');
 
 router.get('/articles', (req, res) => {
     res.send('artigos rota');
 });
-router.get('/admin/articles/new', (req, res) => {
-    res.send('criar artigo');
+router.get("/admin/articles/new", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", {categories: categories});
+    });
 });
-
 module.exports = router;
