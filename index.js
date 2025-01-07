3ed7438aa0ded60489472ed118968d03ebc204ba
Lívia <116227611+lihbolivia@users.noreply.github.com>
2025-01-07 01:25:48 -0300

    `Added category routing and navigation to the application`

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ app.get('/', (req, res) => {
             ['id', 'DESC']
         ]
     }).then(articles =>{
-        res.render('index', {articles: articles});
+        Category.findAll().then(categories=>{
+            res.render('index', {articles: articles, categories: categories});
+        })
     })
 });
 
@@ -32,12 +34,32 @@ app.get('/:slug', (req, res)=>{
     
     }).then(Article =>{
         if(Article!=undefined){
-            res.render('article', {article: Article});
-        }
+            Category.findAll().then(categories=>{
+                res.render('article', {article: article, categories: categories});
+            })        }
         else{
             res.redirect('/');
         }
     }).catch(err=> {
+        res.redirect('/');
+    })
+})
+app.get("/category/:slug",(req, res)=>{
+    var slug = req.params.slug;
+    Category.findOne({
+        where:{
+            slug: slug
+        }, include : [{model: Article}]
+    }).then(category =>{
+        if(category!=undefined){
+            Category.findAll().then(categories=>{
+                res.render('index', {articles: category.articles, categories: categories});
+            })
+        }
+        else{
+            res.redirect('/');
+        }
+    }).catch(err=>{
         res.redirect('/');
     })
 })
