d262571a2ca0ad857cc543f832096dd5603d6125
Lívia <116227611+lihbolivia@users.noreply.github.com>
2025-01-24 21:12:29 -0300

    finished the articles update section.

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -53,16 +53,31 @@ router.post('/articles/delete',(req,res)=>{
         res.redirect('/admin/articles');
     }
 })
-router.get('/admin/articles/edit/:id', (req,res)=>{
-    var id = req.params.id;
-    Article.findByPk(id).then(article=>{
-        if(article!=undefined){
-            Category.findAll().then(categories=>{
-                res.render('admin/articles/edit',{categories:categories});
-            })
-        }else{
-            res.redirect('/');
+router.get("/admin/articles/edit/:id", (req, res) => {
+    const id = req.params.id;
+    Article.findByPk(id).then(article => {
+        if(article){
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", {
+                    article: article,
+                    categories: categories
+                });
+            });
         }
+    });
+});
+router.post('/articles/update',(req,res)=>{
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.update({title: title, body: body, categoryId: category, slug: slugify(title)},{
+        where: {
+            id: id
+        }
+    }).then(()=>{
+        res.redirect('/admin/articles');
     }).catch(err =>{
         res.redirect('/');
     })
